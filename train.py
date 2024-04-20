96ee06cca9d132b7d0418f36b1885b79fc31e3a8
arfin-arif <arifmtbd@gmail.com>
2024-04-20 21:55:36 +0600

    [DONE] split the data and normalize

--- train.py
+++ train.py
@@ -2,6 +2,11 @@ import cv2
 import os
 import numpy as np
 from PIL import Image
+import tensorflow as tf
+from tensorflow import keras
+from PIL import Image
+from tensorflow.keras.utils import normalize 
+from sklearn.model_selection import train_test_split
 
 
 image_directory = "datasets/"
@@ -26,3 +31,18 @@ label = []
 process_images(no_tumor_images, image_directory + "no/", 0)
 process_images(yes_tumor_images, image_directory + "yes/", 1)
 
+
+
+dataset = np.array(dataset)
+label = np.array(label)
+
+x_train, x_test, y_train, y_test = train_test_split(dataset, label, test_size=0.2, random_state=0)
+
+
+print("dataset length:", len(dataset))
+print("label length:", len(label))
+print(x_train.shape)
+
+
+x_train = normalize(x_train, axis=1)
+x_test = normalize(x_test, axis=1)
